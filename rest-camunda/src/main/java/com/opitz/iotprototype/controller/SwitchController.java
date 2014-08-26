0f0cc55f020ebcf8b02e0ec666a2b33c17b2cdf1
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2014-08-26 14:55:38 +0200

    solution

--- rest-camunda/src/main/java/com/opitz/iotprototype/controller/SwitchController.java
+++ rest-camunda/src/main/java/com/opitz/iotprototype/controller/SwitchController.java
@@ -2,9 +2,9 @@ package com.opitz.iotprototype.controller;
 
 import com.opitz.iotprototype.entities.ElroPowerPlug;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,93 +18,125 @@ import java.util.List;
 @RequestMapping("/service/switches")
 public class SwitchController {
 
-	/**
-	 * Activate a certain {@link ElroPowerPlug}.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code PUT .../switches/activate/<exampleId><br/>
-	 * </pre>
-	 * 
-	 * @param id
-	 *            certain {@link ElroPowerPlug} id
-	 * @return certain {@link ElroPowerPlug} retreived from the DB after
-	 *         updating the lastKnownState
-	 */
-	public ElroPowerPlug activate(Integer id) {
-		return null;
-	}
+    /**
+     * Activate a certain {@link ElroPowerPlug}.
+     * <p>
+     *
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code PUT .../switches/activate/<exampleId><br/>
+     * </pre>
+     *
+     * @param id
+     *            certain {@link ElroPowerPlug} id
+     * @return certain {@link ElroPowerPlug} retreived from the DB after
+     *         updating the lastKnownState
+     */
+    @ResponseBody
+    @RequestMapping(value = "/activate/{id}", method = RequestMethod.PUT)
+    public ElroPowerPlug activate(@PathVariable("id") Integer id) {
+        ElroPowerPlug dummy = new ElroPowerPlug();
+        dummy.setId(42);
+        dummy.setLabel("testPlug");
+        dummy.setGroupID("01011");
+        dummy.setSwitchID("01101");
+        dummy.setLastKnownState(true);
+        return dummy;
 
-	/**
-	 * Deactivate a certain {@link ElroPowerPlug}.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code PUT .../switches/deactivate/<exampleId><br/>
-	 * </pre>
-	 * 
-	 * @param id
-	 *            certain {@link ElroPowerPlug} id
-	 * @return certain {@link ElroPowerPlug} retreived from the DB after
-	 *         updating the lastKnownState
-	 */
+    }
 
-	public ElroPowerPlug deactivate(Integer id) {
-		return null;
-	}
+    /**
+     * Deactivate a certain {@link ElroPowerPlug}.
+     * <p>
+     *
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code PUT .../switches/deactivate/<exampleId><br/>
+     * </pre>
+     *
+     * @param id
+     *            certain {@link ElroPowerPlug} id
+     * @return certain {@link ElroPowerPlug} retreived from the DB after
+     *         updating the lastKnownState
+     */
+    @ResponseBody
+    @RequestMapping(value = "/deactivate/{id}", method = RequestMethod.PUT)
+    public ElroPowerPlug deactivate(@PathVariable("id") Integer id) {
+        ElroPowerPlug dummy = new ElroPowerPlug();
+        dummy.setId(42);
+        dummy.setLabel("testPlug");
+        dummy.setGroupID("01011");
+        dummy.setSwitchID("01101");
+        dummy.setLastKnownState(false);
+        return dummy;
+    }
 
-	/**
-	 * Retrieve all {@link ElroPowerPlug}s. (TODO available to the requesting user
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code GET .../switches}
-	 * </pre>
-	 * 
-	 * @return {@link List} of {@link ElroPowerPlug}
-	 */
-	public List<ElroPowerPlug> getAll() {
-		return null;
-	}
+    /**
+     * Retrieve all {@link ElroPowerPlug}s. (TODO available to the requesting
+     * user)
+     * <p>
+     *
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code GET .../switches}
+     * </pre>
+     *
+     * @return {@link List} of {@link ElroPowerPlug}
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<ElroPowerPlug> getAll() {
+        List<ElroPowerPlug> dummyList = new ArrayList<>();
 
-	/**
-	 * Create a new {@link ElroPowerPlug}.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code POST .../switches}<br/>
-	 * and {@link ElroPowerPlug} as {@link RequestBody}
-	 * </pre>
-	 * 
-	 * @param newPlug
-	 *            new {@link ElroPowerPlug}
-	 * 
-	 * @return created {@link ElroPowerPlug}
-	 */
+        ElroPowerPlug dummy = new ElroPowerPlug();
+        dummy.setId(42);
+        dummy.setLabel("testPlug");
+        dummy.setGroupID("01011");
+        dummy.setSwitchID("01101");
+        dummy.setLastKnownState(true);
 
-	public ElroPowerPlug create(ElroPowerPlug newPlug) {
-		return null;
-	}
+        dummyList.add(dummy);
+        return dummyList;
+    }
 
-	/**
-	 * Delete {@link ElroPowerPlug} by id from system.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code DELETE .../switches/<exampleId>}
-	 * </pre>
-	 * 
-	 * @param id
-	 *            unique {@link ElroPowerPlug} identifier
-	 *
-	 */
-	public void delete(Integer id){
+    /**
+     * Create a new {@link ElroPowerPlug}.
+     * <p>
+     *
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code POST .../switches}<br/>
+     * and {@link ElroPowerPlug} as {@link RequestBody}
+     * </pre>
+     *
+     * @param newPlug
+     *            new {@link ElroPowerPlug}
+     *
+     * @return created {@link ElroPowerPlug}
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public ElroPowerPlug create(@RequestBody ElroPowerPlug newPlug) {
+        newPlug.setId(42);
+        return newPlug;
+    }
 
-	}
+    /**
+     * Delete {@link ElroPowerPlug} by id from system.
+     * <p>
+     *
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code DELETE .../switches/<exampleId>}
+     * </pre>
+     *
+     * @param id
+     *            unique {@link ElroPowerPlug} identifier
+     *
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Integer id){
+    }
 
 }
--- rest-camunda/src/main/java/com/opitz/iotprototype/controller/UserController.java
+++ rest-camunda/src/main/java/com/opitz/iotprototype/controller/UserController.java
@@ -1,16 +1,17 @@
 package com.opitz.iotprototype.controller;
 
+import com.opitz.iotprototype.entities.NetworkNode;
 import com.opitz.iotprototype.entities.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * {@link User} controller.
- * 
+ * <p/>
  * User: Pascal Date: 03.09.13 Time: 22:06
  */
 
@@ -18,140 +19,156 @@ import java.util.List;
 @RequestMapping("/service/users")
 public class UserController {
 
-	/**
-	 * Set current state of a certain {@link User} by starting a new process
-	 * instance.
-	 * <p>
-	 * S
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code PUT .../users/<exampleUsername>/state/<exampleState>}<br/>
-	 * and {@link User} as {@link RequestBody}
-	 * </pre>
-	 * 
-	 * @param username
-	 *          unique user name
-	 * @param state
-	 *          expected states: online or offline
-	 * @return process instance id
-	 */
+    /**
+     * Set current state of a certain {@link User} by starting a new process instance.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code PUT .../users/<exampleUsername>/state/<exampleState>}<br/>
+     * and {@link User} as {@link RequestBody}
+     * </pre>
+     *
+     * @param username unique user name
+     * @param state    expected states: online or offline
+     * @return process instance id
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{username}/state/{setState}", method = RequestMethod.PUT)
+    public String setUserState(@PathVariable("username") String username,
+                               @PathVariable("setState") String state) {
+        return "User " + username + "now is " + state;
+    }
 
-	public String setUserState(String username, String state) {
-		return null;
-	}
+    /**
+     * Create a new {@link User}.
+     * <p/>
+     * Please note that when adding a new user via rest, there must be a network node supplied with the user. so first
+     * fetch all network nodes from the api, then select the one that should be the one that the user usually carries on
+     * him (a smartphone e.g.) and then add the user with this method.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code POST .../users}<br/>
+     * and {@link User} as {@link RequestBody}
+     * </pre>
+     *
+     * @param user new {@link User}
+     * @return created {@link User}
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public User create(@RequestBody User user) {
 
-	/**
-	 * Create a new {@link User}.
-	 * <p>
-	 * Please note that when adding a new user via rest, there must be a network
-	 * node supplied with the user. so first fetch all network nodes from the api,
-	 * then select the one that should be the one that the user usually carries on
-	 * him (a smartphone e.g.) and then add the user with this method.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code POST .../users}<br/>
-	 * and {@link User} as {@link RequestBody}
-	 * </pre>
-	 * 
-	 * @param user
-	 *          new {@link User}
-	 * 
-	 * @return created {@link User}
-	 */
+        //later in the service, the user will get an ID and this is a dummy ID
+        user.setId(42);
+        return user;
+    }
 
-	public User create(User user) {
-		return null;
-	}
+    /**
+     * Update a {@link User}.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code PUT .../users<br/>
+     * and {@link User} as {@link RequestBody}
+     * </pre>
+     *
+     * @param user {@link User} with new values
+     * @return updated {@link User}
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PUT)
+    public User update(@RequestBody User user) {
 
-	/**
-	 * Update a {@link User}.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code PUT .../users<br/>
-	 * and {@link User} as {@link RequestBody}
-	 * </pre>
-	 * 
-	 * @param user
-	 *          {@link User} with new values
-	 * 
-	 * @return updated {@link User}
-	 */
+        //just returning the user again, later, the service will first update the ressource in the DB and then return the saved value
+        return user;
+    }
 
-	public User update(User user) {
-	return null;
-	}
+    /**
+     * Delete {@link User} by user name.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code DELETE .../users/<exampleUserID>}
+     * </pre>
+     *
+     * @param userID userID
+     * @return true if deletion successful, otherwise false
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{userID}", method = RequestMethod.DELETE)
+    public boolean delete(@PathVariable("userID") Integer userID) {
+        return true;
+    }
 
-	/**
-	 * Delete {@link User} by user id.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code DELETE .../users/<exampleUserID>}
-	 * </pre>
-	 * 
-	 * @param userID
-	 *          user id
-	 * 
-	 * @return true if deletion successful, otherwise false
-	 */
-	public boolean delete(Integer userID) {
-		return false;
-	}
+    /**
+     * Retrieve all {@link User}.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code GET .../users}
+     * </pre>
+     *
+     * @return {@link List} of {@link User}
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> getAll() {
 
-	/**
-	 * Retrieve all {@link User}.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code GET .../users}
-	 * </pre>
-	 * 
-	 * @return {@link List} of {@link User}
-	 */
-	public List<User> getAll() {
-		return null;
-	}
+        //creating some dummy values
+        List<User> list = new ArrayList<>();
+        User dummy = new User();
+        dummy.setId(42);
+        dummy.setPersonalDevice(new NetworkNode("FF:FF:FF:FF:FF:FF", "iottestphone", "192.168.1.101"));
+        dummy.setUsername("iotTestUser");
+        list.add(dummy);
+        return list;
+    }
 
-	/**
-	 * Retrieve all user MAC address (devices).
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code GET .../users/devices}
-	 * </pre>
-	 * 
-	 * @return {@link HashMap} of form Entry(MacAddress, Username)
-	 */
+    /**
+     * Retrieve all user MAC address (devices).
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code GET .../users/devices}
+     * </pre>
+     *
+     * @return {@link HashMap} of form Entry(MacAddress, Username)
+     */
+    @ResponseBody
+    @RequestMapping(value = "/devices", method = RequestMethod.GET)
+    public HashMap<String, String> getAllDevices() {
+        HashMap<String, String> sampleMap = new HashMap<>();
+        sampleMap.put("FF:FF:FF:FF:FF:FF", "iotTestUser");
+        return sampleMap;
+    }
 
-	public HashMap<String, String> getAllDevices() {
-		return null;
-	}
-
-	/**
-	 * Retrieve {@link User} by user name.
-	 * <p>
-	 * 
-	 * <pre>
-	 * <b>REST call example:</b><br/>
-	 * {@code GET .../users/username/<exampleUserName>}
-	 * </pre>
-	 * 
-	 * 
-	 * @param username
-	 *          user name
-	 * 
-	 * @return {@link User}
-	 */
-
-	public User findByUsername(String username) {
-		return null;
-	}
+    /**
+     * Retrieve {@link User} by user name.
+     * <p/>
+     * <p/>
+     * <pre>
+     * <b>REST call example:</b><br/>
+     * {@code GET .../users/username/<exampleUserName>}
+     * </pre>
+     *
+     * @param username user name
+     * @return {@link User}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/username/{username}", method = RequestMethod.GET)
+    public User findByUsername(@PathVariable("username") String username) {
+        User dummy = new User();
+        dummy.setId(42);
+        dummy.setPersonalDevice(new NetworkNode("FF:FF:FF:FF:FF:FF", "iottestphone", "192.168.1.101"));
+        dummy.setUsername("iotTestUser");
+        return dummy;
+    }
 
 }
