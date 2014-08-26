cf591361961c12077363c994012a796f12a7ee37
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2014-08-26 12:05:22 +0200

    solutions

--- common-domain/src/main/java/com/opitz/iotprototype/entities/DeviceGroup.java
+++ common-domain/src/main/java/com/opitz/iotprototype/entities/DeviceGroup.java
@@ -1,7 +1,11 @@
 package com.opitz.iotprototype.entities;
 
-import javax.persistence.Entity;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * User: Pascal Date: 04.04.14 Time: 13:08
@@ -10,5 +14,52 @@ import java.io.Serializable;
 @Entity
 public class DeviceGroup implements Serializable {
 
+    Integer id;
+    String label;
 
+    // initialize to avoid null values
+    Set<User> usersWithAccess = new HashSet<>();
+    Set<ElroPowerPlug> elroPowerPlugs = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic(optional = false)
+    @Column(unique = true)
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    //cause by using hbm save/update in dao
+    @Cascade(value = org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    public Set<User> getUsersWithAccess() {
+        return usersWithAccess;
+    }
+
+    public void setUsersWithAccess(Set<User> usersWithAccess) {
+        this.usersWithAccess = usersWithAccess;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    // cause by using hbm save/update in dao
+    @Cascade(value = org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    public Set<ElroPowerPlug> getElroPowerPlugs() {
+        return elroPowerPlugs;
+    }
+
+    public void setElroPowerPlugs(Set<ElroPowerPlug> elroPowerPlugs) {
+        this.elroPowerPlugs = elroPowerPlugs;
+    }
 }
--- common-domain/src/main/java/com/opitz/iotprototype/entities/NetworkNode.java
+++ common-domain/src/main/java/com/opitz/iotprototype/entities/NetworkNode.java
@@ -1,7 +1,10 @@
 package com.opitz.iotprototype.entities;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * User: Pascal Date: 06.03.14 Time: 15:17
@@ -9,45 +12,92 @@ import java.io.Serializable;
 @Entity
 public class NetworkNode implements Serializable {
 
+    /**
+     * duplicate can be found in oracle-cep. Please make sure to update any new required fields added to this class in the oracle-cep module as well.
+     */
+    private String macAddress;
+    private String dnsName;
+    private String lastKnownIPAddress;
+    private String userLabel;
+    private Date lastSeen;
 
+    public NetworkNode(String macAddress, String dnsName, String ipAddress) {
+        this.macAddress = macAddress;
+        this.dnsName = dnsName;
+        this.lastKnownIPAddress = ipAddress;
+        this.lastSeen = new Date();
+    }
 
+    public NetworkNode() {
+    }
 
-	/* UNCOMMENT ONCE COMPLETED
+    @Override
+    /**
+     * equals disregards the last seen timestamp but compares the rest. last seen is only updated every 30 minutes via the cron job.
+     */
+    public boolean equals(Object o) {
+        NetworkNode anotherNode;
+        if (o == null) {
+            return false;
+        } else {
+            anotherNode = (NetworkNode) o;
+        }
 
-	public NetworkNode(String macAddress, String dnsName, String ipAddress) {
-		this.macAddress = macAddress;
-		this.dnsName = dnsName;
-		this.lastKnownIPAddress = ipAddress;
-		this.lastSeen = new Date();
-	}
+        boolean mac = anotherNode.macAddress == this.macAddress;
+        boolean ip = anotherNode.lastKnownIPAddress == this.lastKnownIPAddress;
+        boolean dns = anotherNode.dnsName == this.dnsName;
 
-	public NetworkNode() {
-	}
+        if (anotherNode.macAddress == this.macAddress
+                && anotherNode.dnsName == this.dnsName
+                && anotherNode.lastKnownIPAddress == this.lastKnownIPAddress) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	*//**
-	 * equals disregards the last seen timestamp but compares the rest. last seen is only updated every 30 minutes via the cron job.
-	 *//*
-	public boolean equals(Object o) {
-		NetworkNode anotherNode;
-		if (o == null) {
-			return false;
-		} else {
-			anotherNode = (NetworkNode) o;
-		}
+    @Id
+    public String getMacAddress() {
+        return macAddress;
+    }
 
-		boolean mac = anotherNode.macAddress == this.macAddress;
-		boolean ip = anotherNode.lastKnownIPAddress == this.lastKnownIPAddress;
-		boolean dns = anotherNode.dnsName == this.dnsName;
+    private void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
 
-		if (anotherNode.macAddress == this.macAddress
-				&& anotherNode.dnsName == this.dnsName
-				&& anotherNode.lastKnownIPAddress == this.lastKnownIPAddress) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-*/
+    @Basic(optional = true)
+    public String getDnsName() {
+        return dnsName;
+    }
 
+    public void setDnsName(String dnsName) {
+        this.dnsName = dnsName;
+    }
+
+    @Basic(optional = true)
+    public String getLastKnownIPAddress() {
+        return lastKnownIPAddress;
+    }
+
+    public void setLastKnownIPAddress(String lastKnownIPAddress) {
+        this.lastKnownIPAddress = lastKnownIPAddress;
+    }
+
+    @Basic(optional = true)
+    public String getUserLabel() {
+        return userLabel;
+    }
+
+    public void setUserLabel(String userLabel) {
+        this.userLabel = userLabel;
+    }
+
+    @Basic(optional = false)
+    public Date getLastSeen() {
+        return lastSeen;
+    }
+
+    public void setLastSeen(Date lastSeen) {
+        this.lastSeen = lastSeen;
+    }
 }
