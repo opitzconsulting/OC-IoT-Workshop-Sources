21c1ac2a3769227ae8f4467318452a416f7a84ba
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2014-08-27 09:30:27 +0200

    solution

--- spring-service/src/main/java/com/opitz/iotprototype/services/ElroPowerPlugServiceImpl.java
+++ spring-service/src/main/java/com/opitz/iotprototype/services/ElroPowerPlugServiceImpl.java
@@ -3,6 +3,7 @@ package com.opitz.iotprototype.services;
 import com.opitz.iotprototype.daos.ElroPowerPlugDAO;
 import com.opitz.iotprototype.entities.ElroPowerPlug;
 import com.opitz.iotprototype.utils.DataNotFoundException;
+import com.opitz.jni.NativeRCSwitchAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,23 @@ public class ElroPowerPlugServiceImpl implements ElroPowerPlugService {
     @Override
     public ElroPowerPlug setState(ElroPowerPlug elroPowerPlug, boolean state) {
 
-        return null;
+        NativeRCSwitchAdapter jniAdapter= NativeRCSwitchAdapter.getInstance();
+        System.out.println("++++++++" + state + " setting to Switch: " + elroPowerPlug.getGroupID() + " " + elroPowerPlug.getSwitchID());
+
+        if(NativeRCSwitchAdapter.isWorking()){  //if native code is working perform switching, else do nothing but return the current plug from DB
+            if(state){
+                jniAdapter.switchOn(elroPowerPlug.getGroupID(), elroPowerPlug.getSwitchID());
+            }
+            else{
+                jniAdapter.switchOff(elroPowerPlug.getGroupID(), elroPowerPlug.getSwitchID());
+            }
+
+            // after sending the Signal, we set the new "last known state"
+            elroPowerPlug.setLastKnownState(state);
+            save(elroPowerPlug);    //saves the current last known state of the plug, of course only if the signal went through
+        }
+
+        return elroPowerPlugDAO.load(elroPowerPlug.getId());
 
     }
 
