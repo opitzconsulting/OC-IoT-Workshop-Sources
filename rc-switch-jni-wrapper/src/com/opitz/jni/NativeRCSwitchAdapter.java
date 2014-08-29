eeaafd893d333accd47c7ec3c67b6cfa6d6d746c
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2014-08-29 15:10:25 +0200

    added java solution

--- rc-switch-jni-wrapper/src/com/opitz/jni/NativeRCSwitchAdapter.java
+++ rc-switch-jni-wrapper/src/com/opitz/jni/NativeRCSwitchAdapter.java
@@ -31,18 +31,24 @@ public class NativeRCSwitchAdapter {
             Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
             fieldSysPath.setAccessible(true);
             fieldSysPath.set(null, null);
-        } catch (Exception e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
         try {
-            // TODO load the native libraries here. set workingNativeCode to true if successful
+            System.out.println("Loading Native Libraries:: Path: " + System.getProperty("java.library.path"));
+            //loading a library, there are two ways. but it only gets loaded once so doesnt matter if done twice here
+            System.loadLibrary("RCSwitchAdapter");
+            System.load("/usr/local/lib/libRCSwitchAdapter.so");
+            System.out.println("Loading Native Libraries:: loading librarys worked");
+            workingNativeCode = true;
         } catch (UnsatisfiedLinkError e) {
-            System.out.println("## " + e.getMessage() + " ##");
+            e.printStackTrace();
         }
 
     }
 
-    //TODO add the method stubs for the native interfaces
+    public native void switchOn(String group, String unit);
 
+    public native void switchOff(String group, String unit);
 }
